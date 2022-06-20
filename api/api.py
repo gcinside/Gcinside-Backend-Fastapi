25bbea2ff929adb3a5ff0d4e0039c3e974058830
Super <juwoon7163@gmail.com>
2022-06-20 11:53:01 +0900

    add feature username

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from .google import google_login, google_callback
 from .github import github_login, github_callback
 from .auth import refresh
+from .user import username
 
 api_router = APIRouter()
 
@@ -12,3 +13,5 @@ api_router.include_router(github_login.router, prefix="/github", tags=["github_a
 api_router.include_router(github_callback.router, prefix="/github", tags=["github_auth"])
 
 api_router.include_router(refresh.router, prefix="/auth", tags=["auth"])
+
+api_router.include_router(username.router, prefix="/user", tags=["user"])
--- api/user/username.py
+++ api/user/username.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, Depends
+from fastapi_sqlalchemy import db
 from utils.verify_token import verify_token
+from utils.get_subject import get_subject
 from schemas.user import Username
+from models.account_user import AccountUser
 
 
 router = APIRouter()
 
 
-@router.put("/username", dependencies=[Depends(verify_token)])
-async def username(username: Username):
-    pass
+@router.put("/username")
+async def username(username: Username, token: str = Depends(verify_token)):
+    subject = await get_subject(token)
+    db.session.query(AccountUser).filter_by(user_email=subject).update({"user_name": username.username})
+    db.session.commit()
+    return {"message": "Update username"}
