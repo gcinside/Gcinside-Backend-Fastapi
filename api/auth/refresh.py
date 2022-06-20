b21c4543879c8e1ad3868be9732f63d903ed5fc1
Super <juwoon7163@gmail.com>
2022-06-20 11:07:57 +0900

    access_token refresh

--- api/auth/refresh.py
+++ api/auth/refresh.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, HTTPException
 from schemas.auth import Token
 from core.config import settings
+from utils.generate_token import generate_token
 import jwt
 
 
@@ -10,16 +11,12 @@ router = APIRouter()
 @router.post("/refresh")
 async def username(token: Token):
     try:
-        a = jwt.decode(token.access_token, settings.JWT_SECRET, algorithms=settings.JWT_ALGORITHM)
-        b = jwt.decode(token.refresh_token, settings.JWT_SECRET, algorithms=settings.JWT_ALGORITHM)
-
-        print(a, b)
+        sub = jwt.decode(token.refresh_token, settings.JWT_SECRET, algorithms=settings.JWT_ALGORITHM)["sub"]
+        token = generate_token({"sub": sub}, "access")
+        return {"access_token": token}
 
     except jwt.ExpiredSignatureError:
-        pass
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
 
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-
-    else:
-        return
--- utils/generate_token.py
+++ utils/generate_token.py
@@ -7,8 +7,10 @@ def generate_token(payload, token_type):
     if token_type == "access":
         exp = datetime.now() + timedelta(seconds=settings.ACCESS_TOKEN_EXPIRATION_TIME)
     elif token_type == "refresh":
+        payload = {}
         exp = datetime.now() + timedelta(seconds=settings.REFRESH_TOKEN_EXPIRATION_TIME)
 
     payload["exp"] = exp
+    payload["iat"] = datetime.now()
 
     return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
