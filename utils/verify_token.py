78cae717539f06c15c2cb661d22af81283f8eed8
Super <juwoon7163@gmail.com>
2022-06-20 11:08:36 +0900

    blackify

--- utils/verify_token.py
+++ utils/verify_token.py
@@ -6,9 +6,12 @@ import jwt
 async def verify_token(Authorization=Header()):
     try:
         jwt.decode(Authorization, settings.JWT_SECRET, algorithms=settings.JWT_ALGORITHM)
+
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
     else:
         return
