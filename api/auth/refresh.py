3d23c413cb048f2946c00f5f990ffdce409472ca
Super <juwoon7163@gmail.com>
2022-06-21 21:00:18 +0900

    Update 토큰 payload에 타입 추가

--- api/auth/refresh.py
+++ api/auth/refresh.py
@@ -1,17 +1,23 @@
 from fastapi import APIRouter, status, HTTPException
-from schemas.auth import Token
+from schemas.auth import RefreshIn, RefreshOut
 from core.config import settings
 from utils.generate_token import generate_token
+from utils.get_payload_value import get_payload_value
 import jwt
 
 
 router = APIRouter()
 
 
-@router.post("/refresh")
-async def username(token: Token):
+@router.post("/refresh", response_model=RefreshOut)
+async def username(token: RefreshIn):
     try:
-        sub = jwt.decode(token.refresh_token, settings.JWT_SECRET, algorithms=settings.JWT_ALGORITHM)["sub"]
+        token_type = get_payload_value(token.refresh_token, "type")
+        if token_type != "refresh_token":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+        sub = get_payload_value(token.refresh_token, "sub")
+
         token = generate_token({"sub": sub}, "access")
         return {"access_token": token}
 
--- utils/generate_token.py
+++ utils/generate_token.py
@@ -1,16 +1,16 @@
 from core.config import settings
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import jwt
 
 
 def generate_token(payload, token_type):
     if token_type == "access":
-        exp = datetime.now() + timedelta(seconds=settings.ACCESS_TOKEN_EXPIRATION_TIME)
+        exp = datetime.now(timezone(timedelta(hours=9))) + timedelta(seconds=settings.ACCESS_TOKEN_EXPIRATION_TIME)
     elif token_type == "refresh":
-        payload = {}
-        exp = datetime.now() + timedelta(seconds=settings.REFRESH_TOKEN_EXPIRATION_TIME)
+        exp = datetime.now(timezone(timedelta(hours=9))) + timedelta(seconds=settings.REFRESH_TOKEN_EXPIRATION_TIME)
 
+    payload["type"] = f"{token_type}_token"
     payload["exp"] = exp
     payload["iat"] = datetime.now()
 
-    return "Bearer " + jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
+    return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
