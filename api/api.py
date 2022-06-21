b13d628da06bcff432876ef716f19065f5aa72a4
Super <juwoon7163@gmail.com>
2022-06-21 21:00:51 +0900

    .

--- api/api.py
+++ api/api.py
@@ -1,19 +1,19 @@
 from fastapi import APIRouter
-from .google import google_login, google_callback
-from .github import github_login, github_callback
+from .auth.google import google_login, google_callback
+from .auth.github import github_login, github_callback
 from .auth import refresh
 from .user import username, profile_image
 
 
 api_router = APIRouter()
 
-api_router.include_router(google_login.router, prefix="/google", tags=["google_auth"])
-api_router.include_router(google_callback.router, prefix="/google", tags=["google_auth"])
-
-api_router.include_router(github_login.router, prefix="/github", tags=["github_auth"])
-api_router.include_router(github_callback.router, prefix="/github", tags=["github_auth"])
-
 api_router.include_router(username.router, prefix="/user", tags=["user"])
 api_router.include_router(profile_image.router, prefix="/user", tags=["user"])
+
+api_router.include_router(google_login.router, prefix="/google", tags=["auth"])
+api_router.include_router(google_callback.router, prefix="/google", tags=["auth"])
+
+api_router.include_router(github_login.router, prefix="/github", tags=["auth"])
+api_router.include_router(github_callback.router, prefix="/github", tags=["auth"])
 
 api_router.include_router(refresh.router, prefix="/auth", tags=["auth"])
