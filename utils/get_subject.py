e810d33bc17d58f46e5501dfa3a208a77821f90d
Super <juwoon7163@gmail.com>
2022-06-21 21:05:17 +0900

    .

--- utils/get_subject.py
+++
@@ -1,7 +0,0 @@
-from core.config import settings
-import jwt
-
-
-async def get_subject(token: str):
-    sub = jwt.decode(token, settings.JWT_SECRET, algorithms=settings.JWT_ALGORITHM)["sub"]
-    return sub
