799c6b2f33f26fd422675957f79ed53a666a4155
Super <juwoon7163@gmail.com>
2022-06-21 21:08:44 +0900

    .

--- api/api.py
+++ api/api.py
@@ -10,10 +10,10 @@ api_router = APIRouter()
 api_router.include_router(username.router, prefix="/user", tags=["user"])
 api_router.include_router(profile_image.router, prefix="/user", tags=["user"])
 
-api_router.include_router(google_login.router, prefix="/google", tags=["auth"])
-api_router.include_router(google_callback.router, prefix="/google", tags=["auth"])
+api_router.include_router(google_login.router, prefix="/auth/google", tags=["auth"])
+api_router.include_router(google_callback.router, prefix="/auth/google", tags=["auth"])
 
-api_router.include_router(github_login.router, prefix="/github", tags=["auth"])
-api_router.include_router(github_callback.router, prefix="/github", tags=["auth"])
+api_router.include_router(github_login.router, prefix="/auth/github", tags=["auth"])
+api_router.include_router(github_callback.router, prefix="/auth/github", tags=["auth"])
 
 api_router.include_router(refresh.router, prefix="/auth", tags=["auth"])
