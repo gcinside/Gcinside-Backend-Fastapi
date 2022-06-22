50e88ccb6febeaee0091bd48e7c6df1ea4188f37
Super <juwoon7163@gmail.com>
2022-06-22 09:44:16 +0900

    .

--- utils/verify_token.py
+++ utils/verify_token.py
@@ -4,12 +4,13 @@ import jwt
 
 
 async def verify_token(Authorization=Header()):
+    print(Authorization)
     try:
         token = Authorization.split(" ")[1]
         jwt.decode(token, settings.JWT_SECRET, algorithms=settings.JWT_ALGORITHM)
 
     except IndexError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token type")
 
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
